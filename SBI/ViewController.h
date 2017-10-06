f126077f71133ded5a454f79fe5f03b47c7bb3bb
webwerks <webwerks@Ajays-Macbook-Pro.local>
2017-10-06 12:45:38 +0530

    Ïnitial commit by AJAY

---
+++ SBI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SBI
+//
+//  Created by webwerks on 06/10/17.
+//  Copyright © 2017 webwerks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SBI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SBI
+//
+//  Created by webwerks on 06/10/17.
+//  Copyright © 2017 webwerks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
